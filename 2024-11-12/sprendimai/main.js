7c674ee5eb96213b89093b9d0b3834954cf11448
vilius.ramulionis <vilius.ramulionis@gmail.com>
2024-11-13 11:45:29 +0200

    and or not, else if, string methods

---
+++ 2024-11-12/sprendimai/main.js
@@ -0,0 +1,10 @@
+let pirmas = 2;
+let antras = 5;
+
+console.log(`Pirmas skaičius: ${pirmas} Antras skaičius: ${antras}`);
+
+let rezultatas = pirmas + antras;
+
+console.log(`Pirmas skaičius: ${pirmas} Antras skaičius: ${antras} Rezultatas: ${rezultatas}`);
+
+console.log(`${pirmas} + ${antras} = ${pirmas + antras}`);
